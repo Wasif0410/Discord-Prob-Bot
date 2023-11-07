487c89d95b99f82f8224ba6d1a5ce6a2a1d25b2d
Wasif Saeed <115382944+Wasif0410@users.noreply.github.com>
2023-11-07 01:10:47 -0500

    Commit Files

--- bot.py
+++ bot.py
@@ -29,13 +29,13 @@ async def send_message(message, user_message):
     #implemented this so that the text displays after the gif
     #different than the others as the text displays before image for !cards and !flip
     elif(response[0]=="dice"):
-        await message.channel.send(file=discord.File("dice.gif", 'gif.gif'))
+        await message.channel.send(file=discord.File("visuals/dice.gif", 'gif.gif'))
         await asyncio.sleep(1)
         await message.channel.send(f"\n\u200b You Rolled:     ❌   {response[-1]}   ❌")
 
     #handles general cases
     else:
-        gif_path = response[1]
+        gif_path = f"visuals/{response[1]}"
 
         #open as read binary
         with open(gif_path, 'rb') as img:
@@ -48,7 +48,7 @@ async def send_message(message, user_message):
 def run_discord_bot():
 
     #set token value 
-    TOKEN = "MTAzODg2MTE2NDExOTA2NDU4Ng.GUy9bl.f2Jq7dLEq6hX-CsRVCorbuvkqBYyM-MTrKeoLo"
+    TOKEN = "YOUR TOKEN HERE"
 
     #intents
     intents = discord.Intents.all()
@@ -60,7 +60,7 @@ def run_discord_bot():
     async def on_ready():
         print(f"{client.user} is now running")  
 
-    #new event
+    #new event6
     @client.event
     #when a message is sent
     async def on_message(message):
